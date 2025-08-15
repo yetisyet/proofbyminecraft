4df4a62cc19b45be7541106243d21ebecc80be49
quotien <165977773+quotien@users.noreply.github.com>
2025-08-15 22:31:49 +1000

    Update main.py

--- parser/main.py
+++ parser/main.py
@@ -6,39 +6,23 @@ class FOLe():
     def __init__(self, expr):
         body = expr
         pass
-
-def BracketBlock(str):
-    out = [] #list containing blocks
-    counter = 0 #
-    start = 0
-    end = 0
     
-    for (i, c) in enumerate(str):
-        if c == "(":
-            left_counter += 1
-        elif c == ")":
-            right_counter -= 1
-        
-        
-        
+    def BracketBlock(str):
+        out = []
 
+        l_brackets = []
 
+        for i in range(len(str)):
+            if str[i] == "(":
+                l_brackets.append(i)
+            elif str[i] == ")":
+                out.append(str[l_brackets.pop()+1:i])
 
-    return out 
+        return out
 
-def BracketBlock2(str):
-    out = []
+    test1 = "(a^b)v(a^c)"
+    test2 = "(av(bvc))^~(a^(bvc))"
+    edge1 = "a v b"
 
-    l_brackets = []
+    print(f"test1: {BracketBlock2(test1)}, test2: {BracketBlock2(test2)}")
 
-    for i in range(len(str)):
-        if str[i] == "(":
-            l_brackets.append(i)
-        elif str[i] == ")":
-            out.append(str[l_brackets.pop()+1:i])
-
-    return out
-
-test1 = "(a^b)v(a^c)"
-test2 = "(av(bvc))^~(a^(bvc))"
-print(f"test1: {BracketBlock2(test1)}, test2: {BracketBlock2(test2)}")
