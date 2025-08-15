c16755474356c9d65de4beee3cb11d3018cabb57
quotien <165977773+quotien@users.noreply.github.com>
2025-08-15 21:47:24 +1000

    Update main.py

--- parser/main.py
+++ parser/main.py
@@ -7,5 +7,21 @@ class FOLe():
         body = expr
         pass
 
+def BracketBlock(str):
+    out = [] #list containing blocks
+    counter = 0 #
+    start = 0
+    end = 0
+    
+    for (i, c) in enumerate(str):
+        if c == "(":
+            left_counter += 1
+        elif c == ")":
+            right_counter -= 1
+        
+        
+        
 
 
+
+    return out 
