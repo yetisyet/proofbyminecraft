5ab7cbd6b2311f18db1d47b4361611c5d91dd05f
quotien <165977773+quotien@users.noreply.github.com>
2025-08-16 00:22:21 +1000

    Update nodes.py

--- parser/nodes.py
+++ parser/nodes.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 class Node:
     
     def __init__(self):
@@ -6,3 +8,10 @@ class Node:
         # str types refer to variables
         self.left: Node | str = None
         self.right: Node | str = None
+        self.type: Operation = None
+
+class Operation(Enum):
+    NOT = 0
+    AND = 1
+    OR = 2
+
