f37a1d33329df7e129382f3df2757c6d994d5a0a
quotien <165977773+quotien@users.noreply.github.com>
2025-08-17 08:39:35 +1000

    integrated gui and backend idk

--- GUI/test.py
+++
@@ -1,8 +0,0 @@
-# File: logic.py
-
-def process_input(input_text):
-    """This function processes the text passed to it."""
-    # This is where you'll put your logic
-    print("Processing input...")
-    processed_text = f"Processed: {input_text.upper()}"
-    return processed_text
---
+++ src/mainlogic.py
@@ -0,0 +1,40 @@
+from parser import FOLe
+from nodes import Node, Operation
+from worldgen import*
+from arranger import Arranger
+from command import *
+
+def ValidInput(expr) -> bool:
+    if len(expr) < 3:
+        return False
+    
+    var_count = 0
+    op_count = 0
+    bk_count = 0
+
+    for c in expr:
+        if c in ['(',')']:
+            bk_count += 1
+        elif c.isalpha():
+            var_count += 1
+        elif c in ['^','v']:
+            op_count += 1
+    
+    if op_count == var_count - 1 and bk_count%2 == 0:
+        return True
+    else: 
+        return False
+
+def process_input(expr) -> str:
+    """This function processes the text passed to it and returns command
+    for gui shit."""
+
+    try:
+        root = FOLe.CreateGraph(expr)
+        nodes = Arranger.ArrangeGates(root)
+        circuit = Circuit(nodes, (root.position[0], root.position[1]-2), Arranger.ArrangeRedstone(nodes))
+        out = circuit.get_command()
+    except Exception as e:
+        return 'processing error'
+
+    return out
