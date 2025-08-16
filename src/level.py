3fbfc3aa39c88fb4663d245d30b2f8603405a255
quotien <165977773+quotien@users.noreply.github.com>
2025-08-16 21:24:55 +1000

    Update level.py

--- src/level.py
+++ src/level.py
@@ -32,8 +32,8 @@ def GetLctn(node):
 
 #TODO the setting stuff better
 def SetLevels(nodes: list[Node]):
-    for node in nodes:
-        node.level = GetLevel(node)
+    for i in range(len(nodes)):
+        nodes[i].offset = i
     pass
 
 def SetOffset(nodes: list[Node]):
